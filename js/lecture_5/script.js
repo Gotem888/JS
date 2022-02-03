2dcd911abf544ec083d5b380d8685edc93199eb2
Temu4 <artem.vorobjov@gmail.com>
2022-02-03 14:28:48 +0200

    check hw-5

--- js/lecture_5/script.js
+++ js/lecture_5/script.js
@@ -7,8 +7,12 @@ const initialString ='Hello world';
 function searchSubStringIndex(initialString, searchingSubstring){
     const string1 = 'There is no ' + searchingSubstring + ' in the initialString';
     let result = initialString.indexOf(searchingSubstring);
-    if(result>0) return(console.log(result));
-    else return(console.log(string1));
+    if(result>=0) {
+        console.log(result);
+        return;
+    }
+    // you can skip else here
+    console.log(string1);
 }
 
 searchSubStringIndex(initialString, 'world');
@@ -21,8 +25,11 @@ searchSubStringIndex(initialString, 'z');
 function searchSubStringAmount(initialString, searchingSubstring){
     const string1 = 'There is no ' + searchingSubstring + ' in the initialString';
     let result = initialString.split(searchingSubstring).length-1;
-    if(result>0) return(console.log(result));
-    else return(console.log(string1));
+    if(result>0) {
+        console.log(result);
+        return;
+    }
+    console.log(string1);
 }
 
 searchSubStringAmount(initialString,'l');
